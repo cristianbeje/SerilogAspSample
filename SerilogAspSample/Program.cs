8d9c9000bfc15bae32a43b7e171f75d716f8476c
gyula.uszkai <gyula.uszkai@dotwhite.com>
2023-07-26 10:36:30 +0300

    Add Serilog provider

--- SerilogAspSample/Program.cs
+++ SerilogAspSample/Program.cs
@@ -1,6 +1,23 @@
+using Serilog;
+
+//Log.Logger = new LoggerConfiguration()
+//    .WriteTo.Console()
+//    .WriteTo.Debug()
+//    .CreateLogger();
+
+//Log.Information("Starting web application");
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+//builder.Host.UseSerilog();
+builder.Host.UseSerilog((context, services, configuration) => configuration
+    //.ReadFrom.Configuration(context.Configuration)
+    //.ReadFrom.Services(services)
+    .MinimumLevel.Information()
+    .Enrich.FromLogContext()
+    .WriteTo.Debug());
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
